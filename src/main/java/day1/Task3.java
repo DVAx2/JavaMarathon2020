93942274692a1edae4e10cbea19f389ac0dab60a
DVAx2 <dva2507166@gmail.com>
2020-12-24 11:04:57 +0300

    complete day1 tasks

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        int value1 = 0;
+        while (value1 < 10) {
+            System.out.println("JAVA");
+            value1 = value1 + 1;
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,16 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 7;
+     /*   int a = 1;
+        int b = a * k;
+        while (a < 10) {
+            System.out.println(a + " x " + k + " = " + k * a);
+            a = a + 1;
+     */
+        for (int a = 1; a <= 9; a++) {
+            System.out.println(a + " x " + k + " = " + k * a);
+        }
 
     }
 }
