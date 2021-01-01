74685fccc3aa13db93979538601833f963a19a33
DVAx2 <dva2507166@gmail.com>
2021-01-01 13:21:00 +0300

    complete day 5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,51 @@
 package day5;
-
+/*
+Создать класс Автомобиль (англ. Car) с полями “Год выпуска”, “Цвет”, “Модель”.
+Создать get и set методы для каждого поля. Создать экземпляр класса Автомобиль.
+Задать значение для каждого поля, используя set методы.
+Вывести в консоль значение каждого из полей, используя get методы.
+Созданный вами класс должен отвечать принципам инкапсуляции.
+ */
 public class Task1 {
     public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.setYearOfIssue(2021);
+        myCar.setColour("White");
+        myCar.setModel("Кама-1");
+        myCar.getInfo();
+    }
+}
 
+class Car {
+    private int yearOfIssue;
+    private String colour;
+    private String model;
+
+    public void setYearOfIssue(int carYear) {
+        yearOfIssue = carYear;
+    }
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public void setColour(String carColor) {
+        colour = carColor;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setModel(String carModel) {
+        model = carModel;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void getInfo(){
+        System.out.println("Мой автомобиль " + model + "; год выпуска " + yearOfIssue + "; цвет " + colour + ".");
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,30 @@
 package day5;
-
+/*
+Создать класс Мотоцикл (англ. Motorbike) с полями “Год выпуска”, “Цвет”, “Модель”.
+Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
+Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
+Вывести в консоль значение каждого из полей, используя get методы.
+ */
 
 public class Task2 {
     public static void main(String[] args) {
+        Motobike myMotobike = new Motobike("Kawasaki", 2020, "multicolour");
+        myMotobike.getInfo();
+    }
+}
 
+class Motobike {
+    private int yearOfIssue;
+    private String colour;
+    private String model;
+
+    public Motobike(String model, int yearOfIssue, String colour) {
+        this.model = model;
+        this.yearOfIssue = yearOfIssue;
+        this.colour = colour;
+    }
+
+    public void getInfo() {
+        System.out.println("Мой мотоцикл " + model + "; год выпуска " + yearOfIssue + "; цвет " + colour + ".");
     }
 }
